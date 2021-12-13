ceeac095364a8d5138fa7592bf9c519b0aa64dc8
Star-Cyber3 <80070589+Star-Cyber3@users.noreply.github.com>
2021-12-12 21:08:27 -0700

    Add javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+function submitInfo() {
+  const username = document.querySelector("#user").value;
+  const password = document.querySelector("#pass").value;
+  if ((username == "username") & (password == "password")) {
+    alert("ok")
+  } else if ((username == "") || (password == "")) {
+    alert("Error: Please fill in all fields")
+  } else {
+    alert("Error: Username or Password is invalid")
+    document.querySelector("#pass").value = "";
+  }
+}
