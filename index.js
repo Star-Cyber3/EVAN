b641e2cf0759ee6ee030276758a145d0b8b08bb3
Star-Cyber3 <80070589+Star-Cyber3@users.noreply.github.com>
2021-12-14 20:35:51 -0700

    Evan

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+let failedAttempts = 0
+
 function submitInfo() {
-  let username = stringToHash(document.querySelector("#user").value);
-  let password = stringToHash(document.querySelector("#pass").value);
-  alert("password hashed is: " + password);
-  alert("username hashed is: " + username);
-  if ((username == "username") & (password == "password")) {
-    alert("ok")
-  } else if ((username == "") || (password == "")) {
-    alert("Error: Please fill in all fields")
+  let signIn = getCookie("signIn");
+  alert(signIn); //test
+if ((failedAttempts <= 3) || (signIn == "failed")) {
+    let username = stringToHash(document.querySelector("#user").value);
+    let password = stringToHash(document.querySelector("#pass").value);
+    alert("password hashed is: " + password);
+    alert("username hashed is: " + username);
+    if ((username == "284393587") & (password == "-1141017834")) {
+      alert("ok")
+      setCookie("signIn", "correct", 1);
+    } else if ((username == "0") || (password == "0")) {
+      alert("Error: Please fill in all fields")
+      document.querySelector("#pass").value = "";
+    } else {
+      alert("Error: Username or Password is invalid")
+      document.querySelector("#pass").value = "";
+    }
   } else {
-    alert("Error: Username or Password is invalid")
-    document.querySelector("#pass").value = "";
+    setCookie("signIn", "failed", 0.04166666666666666666666666666667);
+    alert("You have failed 3 times to log in. Please wait 1 hour to try again. ")
   }
 }
 
@@ -18,7 +29,9 @@ function stringToHash(string) {
 
   let hash = 0;
 
-  if (string.length == 0) {return hash;}
+  if (string.length == 0) {
+    return hash;
+  }
 
   for (let i = 0; i < string.length; i++) {
     let char = string.charCodeAt(i);
@@ -27,4 +40,27 @@ function stringToHash(string) {
   }
 
   return hash;
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
